74b4f7d9dd613a43e0ca953ebca2f898365917a3
SAFIR jHanm🔥 <121437369+TUNISIA-user@users.noreply.github.com>
2025-06-26 16:50:33 +0100

    Create doc.js
     <button onClick={()=>HanelHelpFunction(item)}>

---
+++ doc.js
@@ -0,0 +1,5 @@
+ <button onClick={()=>HanelHelpFunction(item)}>
+
+this is good thng for when react render the comp  the function not wokring again so this is the best way for handel  this 
+if you type this  <button onClick={()=>HanelHelpFunction(item)}>    like this 
+ <button onClick={HanelHelpFunction(item)}>  +++ 
